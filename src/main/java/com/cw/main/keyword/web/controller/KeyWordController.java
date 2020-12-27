6dc682f81a9a5cad5dc7e61b171fc4574121e0bb
cw-main <wangc@yunzd.com.cn>
2020-12-27 17:35:41 +0800

    端口

--- src/main/java/com/cw/main/keyword/web/controller/KeyWordController.java
+++ src/main/java/com/cw/main/keyword/web/controller/KeyWordController.java
@@ -1,10 +1,13 @@
 package com.cw.main.keyword.web.controller;
 
-import com.cw.main.keyword.web.req.Result;
 import com.cw.main.keyword.service.KeyWordService;
+import com.cw.main.keyword.web.req.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/keyWord")
@@ -13,14 +16,14 @@ public class KeyWordController {
     @Autowired
     private KeyWordService keyWordService;
 
-    @PostMapping("/checkStr")
+    @GetMapping("/checkStr")
     @ResponseBody
     public Result checkStr(@RequestParam("str") String str) {
         return keyWordService.checkStr(str);
     }
 
 
-    @PostMapping("/checkSensitiveWord")
+    @GetMapping("/checkSensitiveWord")
     @ResponseBody
     public Result checkSensitiveWord(@RequestParam("str") String str) {
         return keyWordService.checkSensitiveWord(str);
